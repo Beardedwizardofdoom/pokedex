f361dbfb1f80376e5127cac0d92238ab82840dc2
Beardedwizardofdoom <contact@darylboyce.co.uk>
2018-11-25 19:19:19 +0000

    basic layout messed with in css and html and started looking at array implementation

---
+++ js/script.js
@@ -0,0 +1,35 @@
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+	
+const pokemon = [
+	"cat",
+	"fart",
+	"poop",
+	];
+	document.getElementById('pokegrid').innerHTML = pokemon
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
